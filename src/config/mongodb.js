09a5dc1164a8279556b9f5eba02a0a0c84053bf8
= <=>
2023-11-28 14:27:37 +0700

    init document

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,27 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { env } from '~/config/environment'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstane = null
+const mongoClientInstence = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstence.connect()
+
+  trelloDatabaseInstane = mongoClientInstence.db(env.DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstane) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstane
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstence.close()
+}
